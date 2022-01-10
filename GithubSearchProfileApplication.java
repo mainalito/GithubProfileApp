612b625a245109ce81fa196813322d5fbf32f7af
mainalito <81359755+mainalito@users.noreply.github.com>
2022-01-10 13:17:39 +0300

    githubSearch app

---
+++ GithubSearchProfileApplication.java
@@ -0,0 +1,13 @@
+package com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSearchProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubSearchProfileApplication.class, args);
+	}
+
+}
---
+++ models/Repo.java
@@ -0,0 +1,28 @@
+package com.github.models;
+
+public class Repo {
+
+	public Repo() {
+
+	}
+
+	private String name;
+	private int id;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
